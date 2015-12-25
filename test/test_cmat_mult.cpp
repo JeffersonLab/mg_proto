7fd822d80d23260387621b7a62ad34e05b78ac05
Balint Joo <bjoo@jlab.org>
2015-12-25 05:54:12 -0500

    Added a test to do 8 MVs -- for timing -- need to think of a good correctness test for this. One can specify OMP_NUM_THREADS and in the code set the threads_per_mv threads_per_mv should divide the OMP_NUM_THREADS exactly. This will generate OMP_NUM_THREADS/threads_per_mv groups, each group with threads_per_mv threads in it. Each group is responsible for 1 MV operation.

--- test/test_cmat_mult.cpp
+++ test/test_cmat_mult.cpp
@@ -167,6 +167,7 @@ TEST(CMatMult, TestSpeed)
 	MGUtils::MasterLog(MGUtils::INFO, "Iters=%d CMatMult: Time=%16.8e (sec) Flops = %16.8e (GF)",
 			N_iter, time, gflops_opt);
 
+#if 0
 	// Time Naive
 
 	// Warm cache
@@ -188,12 +189,123 @@ TEST(CMatMult, TestSpeed)
 
 
 	MGUtils::MasterLog(MGUtils::INFO, "Speedup = gflops_opt/gflops_naive=%16.8e", gflops_opt/gflops_naive);
-
+#endif
 
 
 #if 1
 	MGUtils::MemoryFree(y);
 	MGUtils::MemoryFree(y2);
+	MGUtils::MemoryFree(A);
+	MGUtils::MemoryFree(x);
+#endif
+}
+
+TEST(CMatMult, TestSpeed2)
+{
+	const int N = 40;
+	const int N_iter = 1000000;
+	const int N_warm = 200;
+	const int N_dir = 8;
+
+#if 1
+	float *x = static_cast<float*>(MGUtils::MemoryAllocate(N_dir*2*N*sizeof(float)));
+	float *y_dir = static_cast<float*>(MGUtils::MemoryAllocate(N_dir*2*N*sizeof(float)));
+	float *A = static_cast<float*>(MGUtils::MemoryAllocate(N_dir*2*N*N*sizeof(float)));
+	float *y = static_cast<float*>(MGUtils::MemoryAllocate(2*N*sizeof(float)));
+#else
+	__declspec(align(64)) float x[2*N*N_dir];
+	__declspec(align(64)) float y_dir[2*N*N_dir];
+	__declspec(align(64)) float 2[2*N];
+	__declspec(align(64)) float A[2*N*N*N_dir];
+
+#endif
+
+
+	/* Fill A and X with Gaussian Noise */
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::normal_distribution<> d(0,2);
+
+	MGUtils::MasterLog(MGUtils::INFO, "Filling x");
+	for(int dir=0; dir < N_dir; ++dir) {
+		for(int i=0; i < 2*N; ++i) {
+			//x[i] = d(gen);
+			x[i + (2*N)*dir] = (float)(i+(2*N)*dir);
+		}
+	}
+
+	MGUtils::MasterLog(MGUtils::INFO, "Filling A");
+
+	for(int dir=0; dir < N_dir; ++dir) {
+		for(IndexType row=0; row < N; ++row) {
+			for(IndexType col=0; col < N; ++col) {
+				for(IndexType cmplx=0; cmplx < 2; ++cmplx) {
+//					A[(2*N)*row + 2*col + cmplx] = d(gen);
+					A[(2*N*N*dir) + (2*N)*row + 2*col + cmplx] = (float)((2*N*N*dir)+(2*N)*row + 2*col + cmplx);
+				}
+			}
+		}
+	}
+
+	MGUtils::MasterLog(MGUtils::INFO, "Done");
+	double start_time=0;
+	double end_time=0;
+	double time=0;
+	double N_dble = static_cast<double>(N);
+	double N_iter_dble = static_cast<double>(N_iter);
+	double gflops=N_iter_dble*N_dir*(N_dble*(8*N_dble-2))/1.0e9;
+
+#pragma omp parallel shared(y_dir)
+	{
+		const int tid=omp_get_thread_num();
+		const int nthreads=omp_get_num_threads();
+		const int threads_per_mv = 2;
+		const int n_groups = nthreads/threads_per_mv; /* Number of thread teams. Each team works on an MV */
+		const int mv_tid = tid % threads_per_mv; /* thread  within an MV */
+		const int mv_group = tid / threads_per_mv; /* Which MV this thread works on */
+
+
+		// Warm cache
+		for(int iter=0; iter < 1; ++iter ) {
+			for(int dir=mv_group; dir < N_dir; dir+=n_groups) {
+#if 1
+#pragma omp critical
+				{
+					printf("tid=%d mv_tid=%d mv_group=%d dir=%d threads_per_mv=%d\n",
+							tid, mv_tid, mv_group, dir, threads_per_mv);
+				}
+#endif
+				CMatMult(&y_dir[2*N*dir],&A[2*N*N*dir],&x[2*N*dir],N,mv_tid,threads_per_mv);
+			}
+		}
+
+		// Time Optimized
+#pragma omp single
+		{
+			start_time = omp_get_wtime();
+		}
+		for(int iter=0; iter < N_iter; ++iter ) {
+			for(int dir=mv_group; dir < N_dir; dir+=n_groups) {
+				CMatMult(&y_dir[2*N*dir],&A[2*N*N*dir],&x[2*N*dir],N,mv_tid,threads_per_mv);
+			}
+		}
+
+#pragma omp single
+		{
+			end_time = omp_get_wtime();
+		}
+	} // end parallel
+	time=end_time - start_time;
+	double gflops_opt = gflops/time;
+
+	MGUtils::MasterLog(MGUtils::INFO, "Iters=%d CMatMult: Time=%16.8e (sec) Flops = %16.8e (GF)",
+			N_iter, time, gflops_opt);
+
+
+
+#if 1
+	MGUtils::MemoryFree(y);
+	MGUtils::MemoryFree(y_dir);
 	MGUtils::MemoryFree(A);
 	MGUtils::MemoryFree(x);
 #endif
