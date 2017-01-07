199d566b1e52c83ce5949fd67afeef5c9f69f824
bjoo <bjoo@jlab.org>
2017-01-06 22:45:19 -0500

    Refactored thread_limits to its own file.

---
+++ include/lattice/coarse/thread_limits.h
@@ -0,0 +1,31 @@
+/*
+ * thread_limits.h
+ *
+ *  Created on: Jan 6, 2017
+ *      Author: bjoo
+ */
+
+#ifndef INCLUDE_LATTICE_COARSE_THREAD_LIMITS_H_
+#define INCLUDE_LATTICE_COARSE_THREAD_LIMITS_H_
+
+#include "lattice/constants.h"
+
+namespace MG {
+struct ThreadLimits {
+
+
+	IndexType min_vrow;
+	IndexType max_vrow;
+/*
+ *  Was:
+ *    min_row,max_vrow
+ */
+	IndexType min_site;
+	IndexType max_site;
+	unsigned char pad[MG_DEFAULT_CACHE_LINE_SIZE-4*sizeof(IndexType)]; // Cache line pad
+};
+};
+
+
+
+#endif /* INCLUDE_LATTICE_COARSE_THREAD_LIMITS_H_ */
