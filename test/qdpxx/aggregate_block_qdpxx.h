9097559667da861069be16eb5919501e86e5aa7d
bjoo <bjoo@jlab.org>
2017-01-15 12:08:26 -0500

    Added Prototypes for Aggregation/TripleProducts/Etc for Coarse spinors Bodies need to be implemented and tested

--- test/qdpxx/aggregate_block_qdpxx.h
+++ test/qdpxx/aggregate_block_qdpxx.h
@@ -11,6 +11,8 @@
 #include "qdp.h"
 #include "clover_term_qdp_w.h"
 #include "lattice/coarse/coarse_types.h"
+#include "lattice/coarse/coarse_op.h"
+#include <vector>
 
 
 using namespace MG;
@@ -98,6 +100,43 @@ void dslashTripleProductDirQDPXX(const std::vector<Block>& blocklist, int dir, c
 //! Coarsen the clover term (1 block = 1 site )
 void clovTripleProductQDPXX(const std::vector<Block>& blocklist, const QDPCloverTerm& clov,const multi1d<LatticeFermion>& in_vecs, CoarseClover& cl_coarse);
 
+/* --------- COARSE COARSE BLOCK STUFF COming here ------------- */
+//! v *= alpha (alpha is real) over and aggregate in a block, v is a CoarseSpinor
+void axBlockAggr(const double alpha, CoarseSpinor& v, const Block& block, int aggr);
+
+//! y += alpha * x (alpha is complex) over aggregate in a block, x, y are QDP++ LatticeFermions;
+void caxpyBlockAggr(const std::complex<double>& alpha, const CoarseSpinor& x, CoarseSpinor& y,  const Block& block, int aggr);
+
+//! return || v ||^2 over an aggregate in a block, v is a CoarseSpinor
+double norm2BlockAggr(const CoarseSpinor& v, const Block& block, int aggr);
+
+//! return < left | right > = sum left^\dagger_i * right_i for an aggregate, over a block
+std::complex<double>
+innerProductBlockAggr(const CoarseSpinor& left, const CoarseSpinor& right, const Block& block, int aggr);
+
+//! Extract the spins belonging to a given aggregate from QDP++ source vector src, into QDP++ target vector target
+void extractAggregate(CoarseSpinor& target, const CoarseSpinor& src, const Block& block, int aggr );
+
+// ---
+//! Orthonormalize vecs over the spin aggregates within the sites
+//  FIXME: If I want to have a std::vector of a type which has a constructor I need to hold them by pointer.
+//      This is because std::vector<> and multi1d need things which have default constructors. However, objects
+// 		such as CoarseSpinor need to take a reference to a LatticeInfo in construction. Is there a best practice/pattern
+// 		for creating containers of such objects.
+void orthonormalizeBlockAggregates(std::vector<CoarseSpinor*>& vecs, const std::vector<Block>& block_list);
+
+
+//! 'Restrict' a QDP++ spinor to a CoarseSpinor with the same geometry
+void restrictSpinor( const std::vector<Block>& blocklist, const std::vector<CoarseSpinor*>& v, const CoarseSpinor& ferm_in, CoarseSpinor& out);
+
+//! 'Prolongate' a CoarseSpinor to a QDP++ Fine Spinor
+void prolongateSpinor(const std::vector<Block>& blocklist, const std::vector<CoarseSpinor*>& v, const CoarseSpinor& coarse_in, CoarseSpinor& fine_out);
+
+//! Coarsen one direction of a dslash link: FIXME: may become a method of CoarseDiracOp later?
+void dslashTripleProductDir(const CoarseDiracOp& D_op, const std::vector<Block>& blocklist, int dir, const CoarseGauge& u_fine, const std::vector<CoarseSpinor*>& in_vecs, CoarseGauge& u_coarse);
+
+//! Coarsen the clover term (1 block = 1 site ): FIXME: may becme a method of CoarseDiracOp later?
+void clovTripleProduct(const CoarseDiracOp& D_op, const std::vector<Block>& blocklist, const CoarseClover& cl_fine, const std::vector<CoarseSpinor*>& in_vecs, CoarseClover& cl_coarse);
 };
 
 #endif /* TEST_QDPXX_AGGREGATE_QDPXX_H_ */
