b07a8e3ebc4d907d3025fc8dce586a0df7b22cf1
Balint Joo <bjoo@jlab.org>
2017-10-13 10:54:39 -0400

    Removed superfluous files copied over from QPhiX Added QPhiXTypes directory

---
+++ include/lattice/qphix/qphix_types.h
@@ -0,0 +1,22 @@
+/*
+ * qphix_types.h
+ *
+ *  Created on: Oct 13, 2017
+ *      Author: bjoo
+ */
+
+#ifndef INCLUDE_LATTICE_QPHIX_QPHIX_TYPES_H_
+#define INCLUDE_LATTICE_QPHIX_QPHIX_TYPES_H_
+
+namespace MG {
+
+
+
+
+
+}
+
+
+
+
+#endif /* INCLUDE_LATTICE_QPHIX_QPHIX_TYPES_H_ */
