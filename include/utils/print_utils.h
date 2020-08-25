c337ba2be37fe41dad8406a508ab6bb4ee901be2
Eloy Romero <eloy@cs.wm.edu>
2020-08-25 15:42:59 -0400

    fixup! Add monitor progress

--- include/utils/print_utils.h
+++ include/utils/print_utils.h
@@ -5,7 +5,14 @@ namespace MG {
     /*! some log levels */
     enum LogLevel { ERROR = 0, INFO, DEBUG, DEBUG2, DEBUG3 };
 
-    /*! Only Master Process Performs Logging */
+    /**
+     * 	Log only shown by the primary node
+     * 	\param level: if the level is <= the current log level, a message will be printed
+     * 	\param format: printf string format
+     * 	\param variable list of arguments
+     *
+     * 	If level is ERROR, abort the process after showing the error.
+     */
     void MasterLog(LogLevel level, const char *, ...);
 
     /*! All Nodes Perform Logging */
@@ -16,6 +23,26 @@ namespace MG {
 
     /*! Get the current log level */
     LogLevel GetLogLevel(void);
+
+    /**
+     * Abort if no progress is set after monitor_time seconds
+     * \param monitor_time: maximum seconds without progress
+     *
+     * If monitor_time <= 0, then no monitoring is performed.
+     */
+    void SetMonitorTime(int monitor_time);
+
+    /**
+     * Pause monitoring progress
+     */
+
+    void PauseMonitor();
+
+    /**
+     * Set progress
+     */
+
+    void SetProgress();
 }
 
 #endif
